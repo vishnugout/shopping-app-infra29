ccd8e6248bfe15faea2b1d77801ef3393733783f
victor <noah@ihnetworks.com>
2023-12-29 15:09:52 +0000

    first infra commit

---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+project_name     = "shopping"
+project_env      = "prod"
+hosted_zone_id   = "Z086323036DVS2K1LP5RG"
+hosted_zone_name = "devopshero.online"
+hostname         = "shopping"
+
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "instance_type" {}
+variable "project_name" {}
+variable "instance_ami" {}
+variable "project_env" {}
+variable "hosted_zone_name" {}
+variable "hosted_zone_id" {}
+variable "hostname" {}
